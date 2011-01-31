6edbe67803636e5567111b4d52034faacdb4adda
ishaikovsky <ishaikovsky@dcd741e4-2041-11e0-bda6-056c18041c6b>
2011-01-31 19:19:30 +0000

    git-svn-id: https://subversion.exadel.com/exadel/practical_richfaces_book/concord/trunk@11 dcd741e4-2041-11e0-bda6-056c18041c6b

---
+++ panels-demo/src/main/java/org/richfaces/book/examples/tabpanel/CityTabBean.java
@@ -0,0 +1,35 @@
+package org.richfaces.book.examples.tabpanel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
+
+@ManagedBean
+@SessionScoped
+public class CityTabBean {
+
+    private String city = "sf";
+
+    private String selectedTab = "sf";
+
+    public void changeCity(ValueChangeEvent event) {
+
+        selectedTab = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSelectedTab() {
+        return selectedTab;
+    }
+
+    public void setSelectedTab(String selectedTab) {
+        this.selectedTab = selectedTab;
+    }
+}
